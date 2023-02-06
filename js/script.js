31c1821895528198febc2eed1ce24e0dd3166ec1
Yacine667 <sid.yacinee@gmail.com>
2023-02-06 16:08:08 +0100

    add plug-in cluster (regroupé les icones popup )

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 var map = L.map('map').setView([48.584614, 7.7507127], 13);
-var marker = L.marker([48.5497744, 7.7380094]).addTo(map);
-var marker2 = L.marker([48.5576075, 7.7477045]).addTo(map);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '',
 }).addTo(map);
 
-marker.bindPopup("<b>Elan Formation</b><br>03 03 03 03 03");
-marker2.bindPopup("<b>Elan Formation</b><br>03 03 03 03 03");
+
+var markers = L.markerClusterGroup();
+markers.addLayer(L.marker([48.5497744, 7.7380094])).bindPopup("<b>Elan Formation</b><br>03 03 03 03 03");
+markers.addLayer(L.marker([48.5576075, 7.7477045])).bindPopup("<b>Elan Formation</b><br>03 03 03 03 03");
+map.addLayer(markers);
